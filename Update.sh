2b4d2eec666ba4ca547272665427e6f75e4de4e0
Kc <kc@fog.moe>
2024-04-01 17:36:31 +0800

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Use the "chmod +x Update.sh" command in the terminal to give this script executable permissions."
+
+if pgrep -x "snap-store" > /dev/null
+then
+    pid=$(pgrep -x "snap-store")
+    kill $pid
+    echo "Update.sh: snap-store process terminated."
+else
+    echo "Update.sh: snap-store process is not running."
+fi
+
+sudo snap refresh snap-store
+echo "Update.sh: updated snap-store."
